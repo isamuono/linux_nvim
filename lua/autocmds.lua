cefa9f982791f682ca31873f0857f99d4338b25d
isamuono <54347282+isamuono@users.noreply.github.com>
2025-09-07 18:41:41 +0900

    Update autocmds.lua

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,22 +1,31 @@
 local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
--- Remove whitespace on save
-autocmd("BufWritePre", {
-	pattern = "*",
-	command = ":%s/\\s\\+$//e",
+-- Disable autoindent on comment
+autocmd({"BufNewFile", "BufEnter"}, {
+    pattern = "*",
+    --command = "set indentkeys-=0#",
+    callback = function()
+        vim.opt.indentkeys:remove({"0#", "0-", "o", "O"})
+    end,
 })
 
 -- Don't auto commenting new lines
 autocmd("BufEnter", {
-	pattern = "*",
-	command = "set fo-=c fo-=r fo-=o",
+    pattern = "*",
+    command = "set fo-=c fo-=r fo-=o",
+})
+
+-- Remove whitespace on save
+autocmd("BufWritePre", {
+    pattern = "*",
+    command = ":%s/\\s\\+$//e",
 })
 
 -- Restore cursor location when file is opened
-autocmd({ "BufReadPost" }, {
-	pattern = { "*" },
-	callback = function()
-		vim.api.nvim_exec('silent! normal! g`"zv', false)
-	end,
+autocmd({"BufReadPost"}, {
+    pattern = "*",
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
 })
